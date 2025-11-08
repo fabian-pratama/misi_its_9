88796275ad4f49cc69d8bc4803b3ff68a753e2c4
fabian-pratama <fabiuss039@gmail.com>
2025-11-08 20:38:43 +0700

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("rumah")]
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello, welcome to my API!";
+        }
+
+        [HttpGet("hai")]
+
+        public string Hai([FromQuery] string nama)
+        {
+            return "Hello, " + nama + "!";
+        }
+    }
+}
